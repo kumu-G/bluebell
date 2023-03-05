a4b3f7697daa229ec25b7acfe183c5fe11091781
kumu <1710684003@qq.com>
2023-03-05 16:53:36 +0800

    second

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bluebell/dao/mysql"
+	"bluebell/dao/redis"
+	"bluebell/router"
+	"bluebell/settings"
+	"fmt"
+)
 
 /**
  * @Description
@@ -8,14 +14,33 @@ import "fmt"
  * @Date 2023/3/5 13:44
  **/
 func main() {
-	if err := mysql.Init(setting.Conf.MySQLConfig); err != nil {
+	//if len(os.Args) < 2 {
+	//	fmt.Println("need config file.eg: bluebell config.yaml")
+	//	return
+	//}
+	// 加载配置
+	if err := settings.Init("./conf/config.yaml"); err != nil {
+		fmt.Printf("load config failed, err:%v\n", err)
+		return
+	}
+
+	if err := mysql.Init(settings.Conf.MySQLConfig); err != nil {
 		fmt.Printf("init mysql failed, err:%v\n", err)
 		return
 	}
 	defer mysql.Close() // 程序退出关闭数据库连接
-	if err := redis.Init(setting.Conf.RedisConfig); err != nil {
+	if err := redis.Init(settings.Conf.RedisConfig); err != nil {
 		fmt.Printf("init redis failed, err:%v\n", err)
 		return
 	}
 	defer redis.Close()
+
+	// 注册路由
+	r := router.SetupRouter(settings.Conf.Mode)
+	err := r.Run(fmt.Sprintf(":%d", settings.Conf.Port))
+	if err != nil {
+		fmt.Printf("run server failed, err:%v\n", err)
+		return
+	}
+
 }
---
+++ router/route.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"bluebell/logger"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(mode string) *gin.Engine {
+	if mode == gin.ReleaseMode {
+		gin.SetMode(gin.ReleaseMode) // gin设置成发布模式
+	}
+	r := gin.New()
+	//r.Use(logger.GinLogger(), logger.GinRecovery(true), middlewares.RateLimitMiddleware(2*time.Second, 1))
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+}
